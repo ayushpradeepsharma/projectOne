3745ec9d448b2b0349b543290058c0c6e4ea6825
ayush sharma <ayush89able@gmail.com>
2018-09-06 13:07:47 +0530

    changes as per client in login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -9,6 +9,14 @@ import { HomePage } from '../home/home';
  
 import { FirebaseProvider } from '../../providers/firebase/firebase';
 import { GlobalsProvider } from '../../providers/globals/globals';
+
+import {SMS} from '@ionic-native/sms';
+import { EmailValidator } from '../../validators/email';
+
+
+import { TermsOfServicePage } from '../terms-of-service/terms-of-service';
+import { PrivacyPoliciesPage } from '../privacy-policies/privacy-policies';
+import { ContentPoliciesPage } from '../content-policies/content-policies';
 /**
  * Generated class for the LoginPage page.
  *
@@ -28,11 +36,23 @@ export class LoginPage {
   user: {email?:string,password?:string}={};
 
   segment: any;
+
+  User:{ name?:any, email?:any, phonenumber?:any, password?:any, checkcondition?:any } ={};
+  public signupForm;
+  returnInvalid: boolean = false;
+  checkedBoolean: boolean = false;
+  OTPGenerated:number;
+  OTPEntered:number;
+  errormessage: any;
   
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder:FormBuilder,
               public modalCtrl:ModalController,public fireData:FirebaseProvider,public globals:GlobalsProvider,
-              public alertCtrl:AlertController) {
+              public alertCtrl:AlertController,private sms:SMS) {
+    
+    this.segment="signIn"
     this.initializeLoginForm();
+    this.initializeForm();
+    this.User.checkcondition=false;
   }
 
   ionViewDidLoad() {
@@ -107,6 +127,137 @@ export class LoginPage {
   {
     console.log("clicked on create a user");
     this.navCtrl.setRoot(RegisterPage);
+  }
+  
+
+
+  initializeForm() {
+		this.signupForm = this.formBuilder.group({
+			email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])],
+      name: ['',Validators.compose([Validators.required])],
+      phonenumber: ['',Validators.compose([Validators.required,Validators.minLength(10),Validators.maxLength(10)])],
+      checkcondition:['',Validators.compose([Validators.required])],
+		});
+  }
+  
+  clearErrors() {
+		if (this.user.email == "" || this.user.password == "") {
+			this.returnInvalid = false;
+		}
+  }
+  
+  async RegisterUser()
+  {
+    console.log("Clicked on Submit");
+    console.log(this.signupForm.value);
+    // console.log( Math.floor(100000 + Math.random() * 900000) );
+    this.OTPGenerated = Math.floor(100000 + Math.random() * 900000);
+    console.log("OTP=",this.OTPGenerated);
+    this.createAlert(); //remove this afterwards
+    this.sms.send(String(this.signupForm.value.phonenumber),'ayush').then((result)=>{
+      console.log('send');
+      this.createAlert();
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  createAlert()
+  {
+    let alert=this.alertCtrl.create({
+      title:'Enter OTP',
+      inputs:[
+        {
+          name:'OTPEntered',
+          placeholder:'******',
+          type:'number'
+        }
+      ],
+      buttons:[
+        {
+          text:'Submit',
+          handler:(data)=>{
+            console.log("In handler");
+            console.log(JSON.stringify(data));
+            this.OTPEntered=data.OTPEntered;
+            console.log("OTPEntered=",this.OTPEntered);
+            console.log("OTPGenerated=",this.OTPGenerated);
+            if(this.OTPEntered==this.OTPGenerated)
+            {
+              console.log("Correct");
+              this.createUser();
+            }
+            else
+            {
+              console.log("Incorrect");
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  checkboxClicked()
+  {
+    if(this.User.checkcondition==true)
+    {
+      this.checkedBoolean=true;
+    }
+    else
+    {
+      this.checkedBoolean=false;
+    }
+    console.log(this.checkedBoolean);
+  }
+
+  createUser()
+  {
+    this.fireData.signupUser(this.signupForm.value.email,this.signupForm.value.password,
+      this.signupForm.value.name,this.signupForm.value.phonenumber).then((data)=>{
+        console.log(data);
+        this.createSuccessfulAlert();
+        
+    })
+  }
+
+  createSuccessfulAlert()
+  {
+    let alert=this.alertCtrl.create({
+      title:'Success!',
+      message:'User is created Successfully',
+      buttons:[
+        {
+          text:'Ok',
+          handler:()=>{
+            this.navCtrl.setRoot(HomePage);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  tapOnPrivacyPolicies()
+  {
+    console.log('tapped on privacy policies');
+    var modal=this.modalCtrl.create(PrivacyPoliciesPage);
+    modal.present();
+  }
+
+  tapOnTermsOfService()
+  {
+    console.log('tapped on terms of service');
+    var modal= this.modalCtrl.create(TermsOfServicePage);
+    modal.present();
+  }
+
+  tapOnContentPolicies()
+  {
+    console.log('tapped on Content Policies');
+    var modal=this.modalCtrl.create(ContentPoliciesPage);
+    modal.present();
   }
 
 }
